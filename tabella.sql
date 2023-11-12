c7490b8b9c2bdbbf843064348ca60aea9b16a591
fer-ati <144244243+fer-ati@users.noreply.github.com>
2023-11-12 23:32:52 +0000

    Create tabella.sql

---
+++ tabella.sql
@@ -0,0 +1,22 @@
+/*
+ 
+ SQL Create Table
+ Persone By Fer-ati
+ Follow My GitHub
+ https://www.github.com/fer-ati
+ 
+ */
+
+CREATE TABLE IF NOT EXISTS "Persone"(
+
+	"id" INT,
+	"Nome" VARCHAR(35),
+	"Cognome" VARCHAR(20),
+	"Numero" INT,
+	"Indrizzo" VARCHAR(120),
+	"Dettagli" VARCHAR(220)
+
+);
+
+SELECT *
+FROM Prersone;
